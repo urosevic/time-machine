2e05edf078e4d1201ecb0eb2103c1407ef56b553
Aleksandar Urosevic <urke.kg@gmail.com>
2026-09-19 21:18:44 +0200

    Add compiled Gutenberg block

---
+++ blocks/time-machine/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a286be58224b1e21d3ed');
